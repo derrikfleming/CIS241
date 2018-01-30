9887ddcf1838fd44b2f335cff2ff34ad325adb55
derrikf2 <fleminde@mail.gvsu.edu>
2018-01-30 10:54:19 -0500

    finished

--- Lab/lab02/original/cipher.c
+++ Lab/lab02/original/cipher.c
@@ -1,48 +1,40 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 char encrypt(char ch, int k);
 
-int main(int argc, char* argv[]){
-	int choice, key, len;
+int main(int argc, char* argv[])
+{  
+	int choice, key;
 	char ch;
-	int * key_arr;
-	char * str;
 	FILE *fin, *fout;
 
-	if (argc != 5){
+	if (argc != 5)
+	{
 		printf ("Usage: cipher option key infile, outfile\n");
 		printf ("Option 1 for encryption and 2 for decryption");
 		exit(1);
 	}
-
+	
 	choice = atoi(argv[1]);
-	str = argv[2];
-	len = strlen(str);
+	key = atoi(argv[2]);
 
-	key_arr	=	(int	*)	malloc (len	*	sizeof(int));
-
-	for(int i = 0; i < len; i++){
-		key = toupper(str[i]) - 'A';
-
-		if (choice == 2)
-			key = -key;
-
-		key_arr[i] = key;
-	}
-
-	fin = fopen(argv[3], "r");
+	if (choice == 2)
+		key = -key;
+	
+    	fin = fopen(argv[3], "r");
 	fout = fopen(argv[4], "w");
-
-	if (fin ==  NULL || fout == NULL) {
+    
+    	if (fin ==  NULL || fout == NULL) 
+	{
 		printf("File could not be opened\n");
 		exit(1);
 	}
 
-	for(int i = 0; fscanf(fin, "%c", &ch) != EOF; i++){
-		fprintf(fout, "%c", encrypt(ch, key_arr[i % len]));
+	while ( fscanf(fin, "%c", &ch) != EOF )
+	{
+		fprintf(fout, "%c", encrypt(ch, key));
 	}
 
 	fclose(fin);
@@ -51,15 +43,16 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
-char encrypt(char ch, int k){
+char encrypt(char ch, int k)
+{
 	if ( k < 0 )
 		k = k + 26;
 
 	if ( isupper(ch) )
 		return (ch - 'A' + k) % 26 + 'A';
-
+	
 	if ( islower(ch) )
 		return (ch - 'a' + k) % 26 + 'a';
-
+	
 	return ch;
 }
