f4c48429d04ffff6f1c66df676072b0056912c61
derrikf2 <fleminde@mail.gvsu.edu>
2018-02-04 23:15:57 -0500

    finished

--- Projects/Project01/functions.h
+++ Projects/Project01/functions.h
@@ -9,20 +9,26 @@
 
 // remove duplicate characters in array word and return the resulting string
 char * removeDuplicates(char * word);
+
 // search the first num characters in array charArray for character target
 // return a non-zero integer if found, otherwise, return 0
 int targetFound(char * charArray, int num, char target);
+
 // initialize the encrypt array with appropriate cipher letters according to the given key
 void encryptKeyMaker(char * key, char * encrypt);
 
+// initialize decrypt array with appropriate substitute letters based on the encrypt array
 void decryptKeyMaker(char * encrypt, char * decrypt);
+
 // initialize decrypt array with appropriate substitute letters based on the encrypt array
 void processFile(char * substitute, FILE * inf, FILE * outf);
+
 // process data from the input file and write the result to the output file
 // pass the encrypt array to parameter substitute if encryption is intended
 // pass the decrypt array to parameter substitute if decryption is intended
 void decryptFile(char * substitute, FILE * inf, FILE * outf);
 
+//prints out user-friendly error message
 void errorHandler(int errIndex);
 
 #endif //FUNCTIONS_H
