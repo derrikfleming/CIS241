dcf7a0a95bb2aa2e0eb5f75ce61c99157eb56e79
derrikf2 <fleminde@mail.gvsu.edu>
2018-02-20 00:45:04 -0500

    operations

---
+++ Projects/Project02/operations.c
@@ -0,0 +1,43 @@
+#include <operations.h>
+
+// insert a node to the list
+int insert(product **l, product node){
+
+}
+
+// remove a node from list
+void rmItem(product *l, product *node){
+
+}
+
+// show list
+void showList(product *l){
+
+}
+
+// load data from file if
+int loadData(char inf[], product **l){
+
+}
+
+// save data to file outf
+float purchase(product *l, char product[], float q){
+
+}
+
+// check out price of product p from list 1
+void check_price(product *l, char p[]){
+
+}
+
+// find a product p from list l
+void findItem(product *l, char p[]){
+
+}
+
+// the job starts here, shart with laoding data from
+// the file data, and perform the functions by calling
+// related functions. Ends at saving data to the file data
+int doIt(char data[]){
+  
+}
